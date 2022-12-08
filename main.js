fd94bd26cffb543700ad87c62d49efea7256815c
Vitaly05 <vitalik050405@gmail.com>
2022-12-08 13:15:18 +0300

    Added conspect adding button

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ class NavigationPanel {
     }
 
     getHtml() {
-        return `<div id="categories">${this.getCategoriesHtml('/conspects')}<div class="category" id="addCategoryPanel"><button id="addCategoryButton"><i class="fa fa-add"></i><p>Добавить<br />категорию</p></button></div></div>`
+        return `<div id="categories">${this.getCategoriesHtml('/conspects')}<div id="addCategoryPanel"><button class="addButton" id="addCategoryButton"><i class="fa fa-add"></i><p>Добавить<br />категорию</p></button></div></div>`
     }
 
     getCategoriesHtml(path) {
@@ -148,7 +148,7 @@ class Category {
         this.conspects.push(conspect)
     }
     getHtml(path) {
-        return `<div class="category"><div class="categoryTitle"><button class="categoryButton">${this.name}</button><button class="deleteCategoryButton" data-categoryPath="${path}/${this.name}"><i class="fa fa-trash"></i></button></div><div class="conspects">${this.getConspectsHtml(`${path}/${this.name}`)}</div></div>`
+        return `<div class="category"><div class="categoryTitle"><button class="categoryButton">${this.name}</button><button class="deleteCategoryButton" data-categoryPath="${path}/${this.name}"><i class="fa fa-trash"></i></button></div><div class="conspects">${this.getConspectsHtml(`${path}/${this.name}`)}<div id="addPanel"><button class="addButton" id="addConspectButton"><i class="fa fa-add"></i><p>Новый конспект</p></button></div></div></div>`
     }
     getConspectsHtml(path) {
         let html = ''
